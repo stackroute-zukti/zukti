bf3c2053424a7d1460fab1c429df92d02dec56c2
vagrant <mayankaupadhyay@gmail.com>
2017-03-16 07:39:04 +0000

    zukti_redefined

--- webserver/models/userChatHistory.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userChatHistory = mongoose.Schema({
-    email: String,
-    chats: Array
-});
-
-module.exports = mongoose.model('userChatHistory', userChatHistory);
