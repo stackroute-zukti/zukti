c549bc3e5ba32ba7412aa84ff1387da007c76d3a
vagrant <mayankaupadhyay@gmail.com>
2017-03-28 09:43:16 +0000

    zukti test 12

--- webserver/routes/redis/functions/redisClient.js
+++ webserver/routes/redis/functions/redisClient.js
@@ -1,3 +1,3 @@
 /* @navinprasad: create redis client */
-let redis = require('redis').createClient('6380', '127.0.0.1');
+let redis = require('redis').createClient('8888', '127.0.0.1');
 module.exports = redis;
